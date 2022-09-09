f580aaccd1198d3412891c5cd639a15fad171f61
ryan-vimba <rvimba@gmail.com>
2022-09-09 16:58:50 -0400

    split app into multiple components

--- src/App.js
+++ src/App.js
@@ -19,28 +19,43 @@ const list = [
   }
 ]
 
+function Search() {
+  return (
+      <div>
+        <label htmlFor="search">Search: </label>
+        <input id="search" type="text"/>
+      </div>
+  );
+}
+
+function Item(item) {
+  return (
+    <li key={item.objectID}>
+      <span><a href={item.url}>{item.title}</a></span>
+      <span> {item.author} </span>
+      <span>{item.num_comments} </span>
+      <span>{item.points}</span>
+    </li>
+  )
+}
+
+function List() {
+  return (
+    <ul>
+      {list.map(function (item) {
+        return Item(item);
+      })}
+    </ul>
+  );
+}
+
 function App() {
   return (
     <div>
       <h1>My Hacker Stories</h1>
-      
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text"/>
-
+      <Search />
       <hr />
-
-      <ul>
-      {list.map(function (item) {
-        return (
-          <li key={item.objectID}>
-            <span><a href={item.url}>{item.title}</a></span>
-            <span> {item.author} </span>
-            <span>{item.num_comments} </span>
-            <span>{item.points}</span>
-          </li>
-        );
-      })}
-      </ul>
+      <List />
     </div>
   );
 }
