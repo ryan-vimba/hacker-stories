ecc79d0b6cff5477ac30a5e316baed22fc669a7e
ryan-vimba <rvimba@gmail.com>
2022-09-09 17:07:07 -0400

    moved list object into list component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,5 @@
 import * as React from "react";
 
-const list = [
-  {
-    title: "React",
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: "Redux",
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  }
-]
-
 function Search() {
   return (
       <div>
@@ -40,6 +21,26 @@ function Item(item) {
 }
 
 function List() {
+  
+  const list = [
+    {
+      title: "React",
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    }
+  ]
+
   return (
     <ul>
       {list.map(function (item) {
