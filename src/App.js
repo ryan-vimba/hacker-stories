88484394c9d4c58fa656f1a341ce8fc8d4fe3610
ryan-vimba <rvimba@gmail.com>
2022-09-12 17:30:58 -0400

    switched funcs w/o block body to arrow funcs

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import * as React from "react";
 
-function Search() {
-  return (
-      <div>
-        <label htmlFor="search">Search: </label>
-        <input id="search" type="text"/>
-      </div>
-  );
-}
+// function Search() {
+//   return (
+//       <div>
+//         <label htmlFor="search">Search: </label>
+//         <input id="search" type="text"/>
+//       </div>
+//   );
+// }
 
-function Item(item) {
-  return (
-    <li key={item.objectID}>
-      <span><a href={item.url}>{item.title}</a></span>
-      <span> {item.author} </span>
-      <span>{item.num_comments} </span>
-      <span>{item.points}</span>
-    </li>
-  )
-}
+const Search = () => (
+  <div>
+    <label htmlFor="search">Search: </label>
+    <input id="search" type="text"/>
+  </div>
+);
+
+const Item = (item) => (
+  <li key={item.objectID}>
+    <span><a href={item.url}>{item.title}</a></span>
+    <span> {item.author} </span>
+    <span>{item.num_comments} </span>
+    <span>{item.points}</span>
+  </li>
+);
 
 function List() {
   
@@ -43,22 +48,22 @@ function List() {
 
   return (
     <ul>
-      {list.map(function (item) {
+      {list.map((item) => {
         return Item(item);
       })}
     </ul>
   );
 }
 
-function App() {
-  return (
-    <div>
-      <h1>My Hacker Stories</h1>
-      <Search />
-      <hr />
-      <List />
-    </div>
-  );
-}
+const App = () => (
+  <div>
+    <h1>My Hacker Stories</h1>
+    <Search />
+    <hr />
+    <List />
+    <hr />
+    <List />
+  </div>
+);
 
 export default App;
