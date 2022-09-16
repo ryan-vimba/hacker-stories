37924fed55d26ab573a0444ff2d273d5785ade43
ryan-vimba <rvimba@gmail.com>
2022-09-16 13:09:56 -0400

    imperative programming example in react

--- src/App.js
+++ src/App.js
@@ -42,12 +42,22 @@ const App = () => {
   );
 };
 
-const InputWithLabel = ({ id, value, type='text', onInputChange, children }) => (
-  <>
-    <label htmlFor={id}>{children}</label>
-    <input id={id} type={type} value={value} onChange={onInputChange}/>
-  </>
-);
+const InputWithLabel = ({ id, value, type='text', onInputChange, isFocused, children }) => {
+  const inputRef = React.useRef();
+
+  React.useEffect(() => {
+    if (isFocused && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [isFocused]);
+
+  return (
+    <>
+      <label htmlFor={id}>{children}</label>
+      <input ref={inputRef} id={id} type={type} value={value} onChange={onInputChange}/>
+    </>
+  );
+}
 
 const List = ({ list }) => (
   <ul>
