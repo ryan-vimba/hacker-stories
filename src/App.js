3a87a7e1b8900b0712823755e55bdfe34a37ba72
ryan-vimba <rvimba@gmail.com>
2022-09-21 19:38:01 -0400

    react advanced states - useReducer hook

--- src/App.js
+++ src/App.js
@@ -25,24 +25,38 @@ const getAsyncStories = () =>
       () => resolve({data: {stories: initialStories}}), 2000)
   );
 
+const storiesReducer = (state, action) => {
+  switch (action.type) {
+    case "SET_STORIES":
+      return action.payload;
+    case "REMOVE_STORY":
+      return state.filter(
+        (story) => action.payload.objectID !== story.objectID
+      );
+    default:
+      throw new Error();
+  }
+};
+
 const App = () => {
   
-  const [stories, setStories] = React.useState([]);
+  const [stories, dispatchStories] = React.useReducer(storiesReducer, []);
   const [isLoading, setIsLoading] = React.useState(false);
   const [isError, setIsError] = React.useState(false);
   const [searchTerm, setSearchTerm] = useSemiPermanentState('search', 'React');
   
   React.useEffect(() => {
     setIsLoading(true);
-    getAsyncStories().then(result => {
-      setStories(result.data.stories);
-      setIsLoading(false);
-    }).catch(() => setIsError(true));
+    getAsyncStories()
+      .then(result => {
+        dispatchStories({type: 'SET_STORIES', payload: result.data.stories});
+        setIsLoading(false);
+      })
+      .catch(() => setIsError(true));
   }, []);
 
   const handleRemoveStory = (item) => {
-    const newStories = stories.filter((story) => item.objectID !== story.objectID);
-    setStories(newStories);
+    dispatchStories({type: "REMOVE_STORY", payload: item});
   };
 
   const handleChange = (event) => {
